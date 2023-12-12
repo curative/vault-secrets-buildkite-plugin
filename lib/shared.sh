8788524bb4dba7e8610a09b3443e0683365cb79c
BoraxTheClean <owencollierridge@curative.com>
2023-12-12 13:34:52 -0800

    Invoke the script using python3

--- lib/shared.sh
+++ lib/shared.sh
@@ -19,7 +19,7 @@ function vault_auth_aws() {
   if [ -n "${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER:-${VAULT_ADDR}}" ]; then
     VAULT_IAM_SERVER_HEADER=$(echo ${BUILDKITE_PLUGIN_VAULT_SECRETS_AUTH_HEADER:-${VAULT_ADDR}} | sed 's/.*:\/\///' | sed 's/:.*//' | sed 's/\/.*//')
   fi
-  signed_request=$(python ${SIGN_REQUEST_FILE_PATH} ${VAULT_IAM_SERVER_HEADER})
+  signed_request=$(python3 ${SIGN_REQUEST_FILE_PATH} ${VAULT_IAM_SERVER_HEADER})
   iam_request_url=$(echo $signed_request | jq -r .iam_request_url)
   iam_request_body=$(echo $signed_request | jq -r .iam_request_body)
   iam_request_headers=$(echo $signed_request | jq -r .iam_request_headers)
